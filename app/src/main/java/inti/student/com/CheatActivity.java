cf26e155819df089378827f4779995a39eb7a4d6
AdrianTai1996 <adriantai84@yahoo.com>
2019-12-08 13:34:29 +0800

    Commit

---
+++ app/src/main/java/inti/student/com/CheatActivity.java
@@ -0,0 +1,15 @@
+package inti.student.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cheat);
+
+    }
+}
